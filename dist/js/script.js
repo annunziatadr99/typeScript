14e0dd6f68b3d2d350e8f901d2bd6f5f2a2fbcd8
annunziatadr99 <anudar99@hotmail.it>
2024-12-11 16:51:04 +0100

    allenamento typescript

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let myName = "Dario ";
+console.log(myName);
+let mySurname = "Annunziata";
+console.log(mySurname);
+let age = 25;
+console.log(age);
---
+++ script.ts
@@ -0,0 +1,6 @@
+let myName = "Dario ";
+console.log(myName);
+let mySurname = "Annunziata";
+console.log(mySurname);
+let age = 25;
+console.log(age);
